6b841a70c3304c1f765cfaae601e05f996ed44df
Alexander Andreevich <sanyageyjopa@gmail.com>
2025-11-27 01:53:56 +0600

    kal

--- src/weatherGetter/suncalc.ts
+++ src/weatherGetter/suncalc.ts
@@ -1,8 +1,8 @@
 import SunCalc from 'suncalc';
 
-export function sunPosition(now: Date) {    
-    const lat = 54.9924;
-    const lon = 73.3686;
+const lon = 73.3686;
+const lat = 54.9924;
+
 
 export function sunPosition(now: Date): string {    
     const times = SunCalc.getTimes(now, lat, lon);
